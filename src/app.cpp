c94ff8ba47c746de23cfcc2412fb4b41a93f693c
Zandiano <126298640+Zandiano@users.noreply.github.com>
2026-07-28 15:13:59 -0300

    audio working

--- src/app.cpp
+++ src/app.cpp
@@ -7,14 +7,14 @@
 #include "imgui.h"
 #include <string>
 #include <vector>
-#include <queue>
+#include <iostream>
 
 namespace OBJ {
 	class Music {
 	private:
 		std::string name = "Unknown";
 		std::string author = "Unknown";
-		UINT64 duration = 0;
+		ma_uint64 duration = 0;
 		UINT64 id;
 		std::string path = "";
 
@@ -35,7 +35,7 @@ namespace OBJ {
 		void SetDuration(UINT64 duration) {
 			this->duration = duration;
 		}
-		void setDuration(UINT64 duration) {
+		void setDuration(ma_uint64 duration) {
 			this->duration = duration;
 		}
 
@@ -45,7 +45,7 @@ namespace OBJ {
 		std::string GetName() {
 			return this->name;
 		}
-		UINT64 GetDuration() {
+		ma_uint64 GetDuration() {
 			return this->duration;
 		}
 		std::string GetPath() {
@@ -104,28 +104,80 @@ namespace OBJ {
 	private:
 		Playlist playlist = Playlist("Default", "Unknown");
 		Music currentMusic = Music("Unknown", "Unknown", "");
-		UINT64 currentTime = 0;
-		bool isPlaying;
+		
+		ma_engine engine = { NULL };
+		ma_sound currentSound = { NULL };
 
-		ma_engine engine;
-		ma_sound currentSound;
-	
+		ma_uint64 currentTime = 0;
+		ma_uint64 cursor = 0;
+		
+		ma_uint64 currentLength = 0;
+		ma_uint64 lengthInFrames = 0;
+
+		bool isPlaying = false;
+		UINT8 volume = 100;
+
+		float sampleRate = 0;
+
 	public:
 		void Play() {
 			if (!isPlaying) {
-				ma_sound_init_from_file(
-					&engine,
-					currentMusic.GetPath().c_str(),
-					0,
-					NULL,
-					NULL,
-					&currentSound
-				);
+				if (ma_sound_start(&currentSound) != MA_SUCCESS) {
+					std::cout << "Failed to start sound: " << currentMusic.GetPath() << std::endl;
+					return;
+				}
 
-				ma_sound_start(&currentSound);
-
-				isPlaying = true;
+				std::cout << "Playing -> " << currentMusic.GetName() << " by " << currentMusic.GetAuthor() << std::endl;
 			}
+		}
+
+		void Stop() {
+			if (isPlaying) {
+				if (ma_sound_stop(&currentSound) != MA_SUCCESS) {
+					std::cout << "Failed to stop sound: " << currentMusic.GetPath() << std::endl;
+					return;
+				}
+				std::cout << "Stopped -> " << currentMusic.GetName() << " by " << currentMusic.GetAuthor() << std::endl;
+			}
+		}
+
+		void UpdateAudio() {
+			sampleRate = ma_engine_get_sample_rate(&engine);
+			ma_sound_get_cursor_in_pcm_frames(&currentSound, &cursor);
+			currentTime = cursor / sampleRate;
+			isPlaying = ma_sound_is_playing(&currentSound);
+
+			if (!isPlaying) {
+				currentTime = 0;
+				currentMusic = Music("NO MUSIC", "Zandiano", "");
+			}
+
+			ma_engine_set_volume(&engine, volume / 100.0f);
+		}
+
+		void SetCurrentMusic(Music music) {
+			ma_sound_uninit(&currentSound);
+			this->currentMusic = music;
+
+			if (ma_sound_init_from_file(&engine, currentMusic.GetPath().c_str(), 0, NULL, NULL, &currentSound) != MA_SUCCESS) {
+				std::cout << "Failed to load sound: " << currentMusic.GetPath() << std::endl;
+			}
+
+			ma_sound_get_length_in_pcm_frames(&currentSound, &lengthInFrames);
+			currentLength = lengthInFrames / sampleRate;
+			this->currentMusic.setDuration(currentLength);
+		}
+
+		Music GetCurrentMusic() {
+			return this->currentMusic;
+		}
+
+		ma_uint64 GetCurrentMusicTime() {
+			return this->currentTime;
+		}
+
+		UINT8* GetVolume() {
+			return &this->volume;
 		}
 
 		ma_engine* GetEngine() {
@@ -134,12 +186,64 @@ namespace OBJ {
 	};
 
 	class Queue {
-	private:
-		std::queue<Music> queue;
+		struct QueueItem {
+			Music music;
+			QueueItem *next;
+			QueueItem *prev;
+		};
 
+	private:
+		QueueItem *head = nullptr;
+		QueueItem *tail = nullptr;
+		QueueItem *current = nullptr;
 
 	public:
+		void QueueMusic(Music music) {
+			if (tail == nullptr) {
+				tail = new QueueItem{ music, nullptr, nullptr };
+				head = tail;
+				current = tail;
 
+				std::cout << "FQueued-> " << music.GetName() << std::endl;
+				return;
+			}
+			
+			QueueItem *newItem = new QueueItem{ music, nullptr, head };
+			head = newItem;
+
+			std::cout << "Queued-> " << music.GetName() << std::endl;
+		}
+
+		void RemoveMusic() {
+			if (!current->next) {
+				std::cout << "No music to remove" << std::endl;
+				return;
+			}
+
+			if (current->prev) {
+				current->prev->next = current;
+				if (head == current) {
+					head == current->next;
+				}
+			}
+			if (current->next) {
+				current->next->prev = current->prev;
+				
+				if (tail == current) {
+					tail == current->next;
+				}
+			}
+
+			std::cout << "Removed-> " << current->music.GetAuthor() << std::endl;
+
+			QueueItem *temp = current->next;
+			delete current;
+			current = temp;
+		}
+
+		void QueueMusicNow(Music music) {
+
+		}
 	};
 }
 
@@ -147,8 +251,9 @@ namespace App {
 
 	class App {
 	private:
-		OBJ::Player player;
 	public:
+		OBJ::Player player;
+
 		void Init() {
 			if (ma_engine_init(NULL, player.GetEngine()) != MA_SUCCESS)
 			{
@@ -193,11 +298,30 @@ namespace App {
 		draw->AddCircleFilled(music_circleCenter, 100.0f, IM_COL32(255, 0, 0, 255));
 		draw->AddCircleFilled(music_circleCenter, 40.0f, IM_COL32(50, 50, 50, 100));
 
-		ImGui::Dummy(ImVec2(0, music_Height - 35));
-		ImGui::Text("MUSICA X");
-		ImGui::SameLine();
-		ImGui::Text("00:00 / 00:00");
+		ImGui::Dummy(ImVec2(0, music_Height - 80));
 
+		if (ImGui::Button("Play")) {
+			OBJ::Music test = OBJ::Music("20th Century Schizoid Man", "King Crimson", "..\\..\\..\\musics\\20thCenturySchizoidMan.mp3");
+			app.player.SetCurrentMusic(test);
+			app.player.Play();
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("Stop")) {
+			app.player.Stop();
+		}
+		
+		ImGui::SetNextItemWidth(200.0f);
+		ImGui::SliderInt("##Volume", (int*)app.player.GetVolume(), 0, 200);
+
+		ImGui::Text(app.player.GetCurrentMusic().GetName().c_str());
+		ImGui::SameLine();
+
+		ma_uint64 totalSeconds = app.player.GetCurrentMusic().GetDuration();
+		ma_uint64 currentSeconds = app.player.GetCurrentMusicTime();
+
+		ImGui::Text("%02d : %02d / %02d : %02d", (int)(currentSeconds / 60), (int)(currentSeconds % 60), (int)(totalSeconds / 60), (int)(totalSeconds % 60));
+
+		app.player.UpdateAudio();
 
 		ImGui::End();
 
